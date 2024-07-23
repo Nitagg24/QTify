a46e91b7f1b927622629a0ee4fdb6d6c95556f8b
Nitin Aggarwal <your-username@users.noreply.github.com>
2024-07-23 13:23:37 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
+import NavBar from "./components/NavBar/NavBar";
+import Hero from "./components/Hero/Hero";
+import GridBox from "./components/GridBox/GridBox";
+import { useEffect, useState } from "react";
+import { fetchTopAlbums, fetchNewAlbums, fetchSongs } from "./api/api";
+import Accordian from "./components/Accordian/Accordian";
+import CurrentSong from "./components/CurrentSongPlaying/CurrentSong";
+import "./App.css";
+import SongsPage from "./components/SongsPage/SongsPage";
 
-import './App.css';
-import Hero from './components/Hero/Hero';
-import Navbar from './components/Navbar/Navbar';
-import Section from './components/Section/Section';
-function App() {
+export default function App() {
+  const [topAlbumsData, setTopAlbumsData] = useState([]);
+  const [newAlbumsData, setNewAlbumsData] = useState([]);
+  const [songs, setSongs] = useState([]);
+  const [totalData, setTotalData] = useState([]);
+  const [currentSong, setCurrentSong] = useState(null);
+
+  useEffect(() => {
+    generateData();
+  }, []);
+
+  const generateData = async () => {
+    const topAlbumsResponse = await fetchTopAlbums();
+    setTopAlbumsData(topAlbumsResponse);
+
+    const newAlbumsResponse = await fetchNewAlbums();
+    setNewAlbumsData(newAlbumsResponse);
+
+    const songsResponse = await fetchSongs();
+    setSongs(songsResponse);
+
+    setTotalData([...topAlbumsResponse, ...newAlbumsResponse]);
+  };
+  console.log(currentSong);
   return (
-    <div className="App">
-     <Navbar/>
-     <Hero/>
-     <Section type='new'/>
-     <Section type='top'/>
-     <Section type='songs'/>
-    </div>
+    <>
+      <NavBar totalData={totalData} />
+      <Hero />
+      <GridBox title="Top Albums" data={topAlbumsData} />
+      <GridBox title="New Albums" data={newAlbumsData} />
+      <GridBox
+        title="Songs"
+        data={songs}
+        type="songs"
+        setCurrentSong={setCurrentSong}
+      />
+      <Accordian />
+      {currentSong && <CurrentSong song={currentSong} />}
+    </>
   );
 }
-
-export default App;
